790c6cb742c28bd881e74c62beaa8fdc1f98b99d
Guillaume Briday <guillaumebriday@gmail.com>
2020-04-04 22:46:02 +0200

    Upgrading gallery with react-images 1.x

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,62 +1,18 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import Lightbox from 'react-images'
+import Carousel, { Modal, ModalGateway } from 'react-images'
 import Img from 'gatsby-image'
 
-class Gallery extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      lightboxIsOpen: false,
-      currentImage: 0,
-    }
-
-    this.closeLightbox = this.closeLightbox.bind(this)
-    this.gotoNext = this.gotoNext.bind(this)
-    this.gotoPrevious = this.gotoPrevious.bind(this)
-    this.gotoImage = this.gotoImage.bind(this)
-    this.handleClickImage = this.handleClickImage.bind(this)
-    this.openLightbox = this.openLightbox.bind(this)
+export default class extends Component {
+  state = {
+    selectedIndex: 0,
+    lightboxIsOpen: false,
   }
 
-  openLightbox(index, event) {
-    event.preventDefault()
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true,
-    })
-  }
-
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false,
-    })
-  }
-
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1,
-    })
-  }
-
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1,
-    })
-  }
-
-  gotoImage(index) {
-    this.setState({
-      currentImage: index,
-    })
-  }
-
-  handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return
-
-    this.gotoNext()
+  toggleLightbox = (selectedIndex) => {
+    this.setState((state) => ({
+      lightboxIsOpen: !state.lightboxIsOpen,
+      selectedIndex,
+    }))
   }
 
   renderGallery() {
@@ -67,9 +23,15 @@ class Gallery extends Component {
     const gallery = images.map((obj, i) => {
       return (
         <article key={i}>
-          <a href={obj.src} onClick={(e) => this.openLightbox(i, e)}>
+          <div
+            className="inline cursor-pointer"
+            onClick={() => this.toggleLightbox(i)}
+            onKeyDown={() => this.toggleLightbox(i)}
+            role="button"
+            tabIndex="0"
+          >
             <Img fluid={obj.fluid} />
-          </a>
+          </div>
         </article>
       )
     })
@@ -78,33 +40,37 @@ class Gallery extends Component {
   }
 
   render() {
+    const { images } = this.props
+    const { selectedIndex, lightboxIsOpen } = this.state
+
     return (
       <>
         <div className="gallery-wrapper">{this.renderGallery()}</div>
 
-        <Lightbox
-          currentImage={this.state.currentImage}
-          images={this.props.images}
-          isOpen={this.state.lightboxIsOpen}
-          onClickImage={this.handleClickImage}
-          onClickNext={this.gotoNext}
-          onClickPrev={this.gotoPrevious}
-          onClickThumbnail={this.gotoImage}
-          onClose={this.closeLightbox}
-          backdropClosesModal={true}
-          imageCountSeparator=" sur "
-          leftArrowTitle="Précédente"
-          rightArrowTitle="Suivante"
-          width="auto"
-        />
+        <ModalGateway>
+          {lightboxIsOpen ? (
+            <Modal onClose={this.toggleLightbox}>
+              <Carousel
+                currentIndex={selectedIndex}
+                frameProps={{ autoSize: 'height' }}
+                views={images}
+                styles={{
+                  view: (base) => ({
+                    ...base,
+                    alignItems: 'center',
+                    display: 'flex ',
+                    height: 'calc(100vh - 54px)',
+                    justifyContent: 'center',
+                    '& > img': {
+                      maxHeight: 'calc(100vh - 94px)',
+                    },
+                  }),
+                }}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </>
     )
   }
 }
-
-Gallery.displayName = 'Gallery'
-Gallery.propTypes = {
-  images: PropTypes.array,
-}
-
-export default Gallery
